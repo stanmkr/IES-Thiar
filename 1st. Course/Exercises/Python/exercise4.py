8abe972b9490782c35d0a8eff0c5fa49ac5022da
Stanislav Krastev <stanis.krastev@gmail.com>
2022-09-08 22:31:30 +0200

    Adding exercise

---
+++ 1st. Course/Exercises/Python/exercise4.py
@@ -0,0 +1,4 @@
+nombre = input("Introdue el nombre de la persona ganadora:")
+cantidad = int(input("Introduce la cantidad de veces que se imprimirá: "))
+frase = "La persona ganadora del premio es: "
+print((frase+nombre)*cantidad)
