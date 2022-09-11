2f74f86f531177a41ad9be300dad660268ddb219
Stanislav Krastev <stanis.krastev@gmail.com>
2022-09-11 19:43:46 +0200

    Adding exercise

---
+++ 1st. Course/Exercises/Python/Exercise47/exercise47.py
@@ -0,0 +1,5 @@
+import modulo
+
+mariaRodriguez = {"Matemáticas": 6.7, "Lengua": 5.6, "Educación Física": 4.3, "Informática": 9.8,
+                  "Geografía e Historia": 7.2, "Inglés": 10, "Plástica": 11}
+modulo.notas(mariaRodriguez)
