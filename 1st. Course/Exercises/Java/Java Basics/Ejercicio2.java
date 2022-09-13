1ee81d2f7223331a4dd1f0a770159a3c7b326ab1
Stanislav Krastev <stanis.krastev@gmail.com>
2022-09-13 08:54:13 +0200

    Adding exercise

---
+++ 1st. Course/Exercises/Java/Java Basics/Ejercicio2.java
@@ -0,0 +1,15 @@
+/**
+ * @author stani
+ * Tema4
+ * @date octubre 2021
+ *
+ * Dibuja un ordinograma que calcule y muestre el área de un cuadrado de lado igual a 5.
+ *
+ */
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        int lado = 5;
+        int area = lado * lado;
+        System.out.println("El área del cuadrado es: " + area);
+    }
+}
