f1603dee73c3db4e1de6cba23f77bcf6709098d8
Stanislav Krastev <stanis.krastev@gmail.com>
2022-09-13 08:52:57 +0200

    Adding exercise

---
+++ 1st. Course/Exercises/Java/Java Basics/Ejercicio1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * @author stani
+ * Tema4
+ * Ejercicio 1
+ * @date octubre 2021
+ *  Dibuja un ordinograma que dé los “buenos días” .
+ */
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        System.out.println("Buenos días!");
+
+    }
+}
