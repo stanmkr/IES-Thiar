93e7db35a527475e1f49f6e90a5233c44368cb9f
Stanislav Krastev <stanis.krastev@gmail.com>
2022-09-14 11:25:04 +0200

    Adding exercise

---
+++ 1st. Course/Exercises/Java/Java Basics v2/Exercises A/Ejercicio1.java
@@ -0,0 +1,15 @@
+package EjerciciosA;
+
+/**
+ * @author stani
+ * Tema5
+ * @date octubre 2021
+ *  Realiza un programa que muestre por pantalla los 20 primeros números naturales (1, 2, 3... 20)
+ */
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20 ; i++) {
+            System.out.println(i);
+        }
+    }
+}
