0c012d89e2927d138a1ab0dcfd44fd9f8260fe47
Stanislav Krastev <stanis.krastev@gmail.com>
2022-09-14 11:25:21 +0200

    Adding exercise

---
+++ 1st. Course/Exercises/Java/Java Basics v2/Exercises A/Ejercicio2.java
@@ -0,0 +1,16 @@
+package EjerciciosA;
+
+/**
+ * @author stani
+ * Tema5
+ * @date octubre 2021
+ * Realiza un programa que muestre los números pares comprendidos entre el 0 y el 200.
+ * Para ello utiliza un contador y suma de 2 en 2.
+ */
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 200 ; i+=2) {
+            System.out.println(i);
+        }
+    }
+}
