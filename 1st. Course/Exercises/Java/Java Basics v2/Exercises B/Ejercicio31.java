94cc802de48054a8964b84cc56a28ae32301475b
Stanislav Krastev <stanis.krastev@gmail.com>
2022-09-16 09:33:47 +0200

    Adding exercise

---
+++ 1st. Course/Exercises/Java/Java Basics v2/Exercises B/Ejercicio31.java
@@ -0,0 +1,11 @@
+package EjerciciosB;
+
+public class Ejercicio31 {
+    public static void main(String[] args) {
+        for (int i = 1; i <=499 ; i++) {
+            if (i%5==0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
