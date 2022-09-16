a5ec2c4e9f77ca994a1e337935004bda5d817283
Stanislav Krastev <stanis.krastev@gmail.com>
2022-09-16 09:32:45 +0200

    Adding exercise

---
+++ 1st. Course/Exercises/Java/Java Basics v2/Exercises B/Ejercicio28.java
@@ -0,0 +1,16 @@
+package EjerciciosB;
+
+import java.util.Scanner;
+
+/**
+ * Developed by Stanislav Krastev 1º DAM
+ * Tema5 - EjerciciosB
+ * noviembre - 2021
+ * Realiza un programa que pida un número y construya por pantalla su pirámide.
+ */
+public class Ejercicio28 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
