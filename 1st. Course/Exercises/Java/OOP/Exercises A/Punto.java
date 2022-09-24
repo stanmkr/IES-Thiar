b73e5725ef4d77854dd6af836563f40303f246a7
Stanislav Krastev <stani.mk22@gmail.com>
2022-09-24 10:38:45 +0200

    Adding exercise

---
+++ 1st. Course/Exercises/Java/OOP/Exercises A/Punto.java
@@ -0,0 +1,18 @@
+package ejercicios.ejerciciosA;
+
+/**
+ * Developed by Stanislav Krastev 1º DAM
+ * Tema8_OOP - ejercicios.ejerciciosA
+ * enero - 2022
+ *
+ * Crea un programa con una clase llamada Punto que representará un punto de dos dimensiones en un plano.
+ * Solo contendrá dos atributos enteros llamadas x e y (coordenadas).
+ *
+ * En el main de la clase principal instancia 3 objetos Punto con las coordenadas (5,0), (10,10) y (-3, 7).
+ * Muestra por pantalla sus coordenadas (utiliza un println para cada punto).
+ * Modifica todas las coordenadas (prueba distintos operadores como = + - += *=...) y vuelve a imprimirlas por pantalla.
+ *
+ */
+public class Punto {
+    int x,y;
+}
