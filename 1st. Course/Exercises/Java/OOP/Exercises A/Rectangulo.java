666eae9a959e9b0a3375c52f0ff15c191b80afbe
Stanislav Krastev <stani.mk22@gmail.com>
2022-09-24 10:39:18 +0200

    Adding exercise

---
+++ 1st. Course/Exercises/Java/OOP/Exercises A/Rectangulo.java
@@ -0,0 +1,18 @@
+package ejercicios.ejerciciosA;/** Developed by Stanislav Krastev 1º DAM
+Tema8_OOP - ejercicios.ejerciciosA
+enero - 2022
+
+ Crea un programa con una clase llamada Rectangulo que representará un rectángulo mediante dos coordenadas (x1,y1) y (x2,y2) en un plano,
+ por lo que la clase deberá tener cuatro atributos enteros: x1, y1, x2, y2.
+
+ En el main de la clase principal instancia 2 objetos Rectangulo en (0,0)(5,5) y (7,9)(2,3).
+ Muestra por pantalla sus coordenadas, perímetros (suma de lados) y áreas (ancho x alto).
+ Modifica todas las coordenadas como consideres y vuelve a imprimir coordenadas, perímetros y áreas.
+
+
+*/
+
+public class Rectangulo {
+int x1,x2,y1,y2;
+
+}
