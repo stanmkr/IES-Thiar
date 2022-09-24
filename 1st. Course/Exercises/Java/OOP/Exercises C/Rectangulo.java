5d2a62302f7cbbdfa32b22a50cb7429e7c625b42
Stanislav Krastev <stani.mk22@gmail.com>
2022-09-24 10:46:36 +0200

    Adding exercise

---
+++ 1st. Course/Exercises/Java/OOP/Exercises C/Rectangulo.java
@@ -0,0 +1,79 @@
+package ejercicios.ejerciciosC;
+
+/**
+ * Developed by Stanislav Krastev 1º DAM
+ * Tema8_OOP - ejercicios.ejerciciosA
+ * enero - 2022
+ * <p>
+ * Crea un programa con una clase llamada Rectangulo que representará un rectángulo mediante dos coordenadas (x1,y1) y (x2,y2) en un plano,
+ * por lo que la clase deberá tener cuatro atributos enteros: x1, y1, x2, y2.
+ * <p>
+ * En el main de la clase principal instancia 2 objetos Rectangulo en (0,0)(5,5) y (7,9)(2,3).
+ * Muestra por pantalla sus coordenadas, perímetros (suma de lados) y áreas (ancho x alto).
+ * Modifica todas las coordenadas como consideres y vuelve a imprimir coordenadas, perímetros y áreas.
+ */
+
+public class Rectangulo {
+    private int x1, x2, y1, y2;
+
+    // constructor
+    public Rectangulo(int x1, int y1, int x2, int y2) {
+        if (x1 < x2 && y1 < y2) {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }else{
+            System.err.println("ERROR al instanciar Rectángulo");
+
+        }
+    }
+
+    public int getX1() {
+        return x1;
+    }
+
+    public void setX1(int x1) {
+        if (x1 < x2) {
+            this.x1 = x1;
+        } else {
+            System.err.println("ERROR, no es posible modificar el valor.");
+        }
+    }
+
+    public int getX2() {
+        return x2;
+    }
+
+    public void setX2(int x2) {
+        if (x2 > x1) {
+            this.x2 = x2;
+        } else {
+            System.err.println("ERROR, no es posible modificar el valor.");
+        }
+    }
+
+    public int getY1() {
+        return y1;
+    }
+
+    public void setY1(int y1) {
+        if (y1 < y2) {
+            this.y1 = y1;
+        } else {
+            System.err.println("ERROR, no es posible modificar el valor.");
+        }
+    }
+
+    public int getY2() {
+        return y2;
+    }
+
+    public void setY2(int y2) {
+        if (y2 > y1) {
+            this.y2 = y2;
+        } else {
+            System.err.println("ERROR, no es posible modificar el valor.");
+        }
+    }
+}
