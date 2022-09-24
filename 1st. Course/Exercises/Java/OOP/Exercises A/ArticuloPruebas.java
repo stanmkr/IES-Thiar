e8224cdfe927ad67d00beade3c0468f6e4a63b23
Stanislav Krastev <stani.mk22@gmail.com>
2022-09-24 10:36:32 +0200

    Adding exercise

---
+++ 1st. Course/Exercises/Java/OOP/Exercises A/ArticuloPruebas.java
@@ -0,0 +1,16 @@
+package ejercicios.ejerciciosA;
+
+/**
+ * Developed by Stanislav Krastev 1º DAM
+ * Tema8_OOP - ejercicios.ejerciciosA
+ * enero - 2022
+ */
+public class ArticuloPruebas {
+    public static void main(String[] args) {
+        Articulo art1 = new Articulo();
+        art1.nombre = "Pijama";
+        art1.precioSinIVA = 15;
+        art1.stock = 5;
+        System.out.printf("%s - Precio:%.2f€ - IVA:%.2f%% - PVP: %.2f", art1.nombre, art1.precioSinIVA, art1.iva, ((art1.precioSinIVA * art1.iva) / 100)+ art1.precioSinIVA);
+    }
+}
