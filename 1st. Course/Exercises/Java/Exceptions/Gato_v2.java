a2a2453e1d454b235f22276801b18a8de8adb6a1
Stanislav Krastev <stani.mk22@gmail.com>
2022-09-29 16:45:56 +0200

    Adding exercise

---
+++ 1st. Course/Exercises/Java/Exceptions/Gato_v2.java
@@ -0,0 +1,43 @@
+/**
+ * Created by Stanislav Krastev
+ * Tema10_Excepciones - PACKAGE_NAME
+ * Date: marzo / 2022
+ */
+public class Gato_v2 {
+    private String nombre;
+    private int edad;
+
+    //  CONSTRUCTOR
+    public Gato_v2(String nombre, int edad) throws Exception {
+        if (nombre.length() < 3) throw new Exception("El nombre del gato debe de tener al menos 3 caracteres");
+        if (edad < 0) throw new Exception("La edad del gato no puede ser negativa");
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+
+    //  GETTERS Y SETTERS
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) throws Exception {
+        if (nombre.length() < 3) throw new Exception("El nombre del gato debe de tener al menos 3 caracteres");
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) throws Exception {
+        if (edad < 0) throw new Exception("La edad del gato no puede ser negativa");
+        this.edad = edad;
+    }
+
+    //  MÉTODOS
+    public void imprimir(){
+        System.out.println("Nombre del gato: " + getNombre());
+        System.out.println("Edad del gato: " + getEdad());
+    }
+}
