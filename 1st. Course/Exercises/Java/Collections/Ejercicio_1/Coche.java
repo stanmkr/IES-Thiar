1f5ab6f52976dd5bb07d892c1c84bf0c27d0d30f
Stanislav Krastev <stani.mk22@gmail.com>
2022-09-29 16:49:34 +0200

    Adding exercise

---
+++ 1st. Course/Exercises/Java/Collections/Ejercicio_1/Coche.java
@@ -0,0 +1,18 @@
+package Ejercicio_1;
+
+/**
+ * Created by Stanislav Krastev
+ * TEMA11_Colecciones - Ejercicios
+ * Date: marzo / 2022
+ */
+public abstract class Coche {
+    private String matricula;
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+}
