8092212c747ccf193da4bdbe556cf171623a6bdf
Stanislav Krastev <stani.mk22@gmail.com>
2022-09-30 16:57:07 +0200

    Adding exercise

---
+++ 1st. Course/Exercises/Java/Collections/Ejercicio_5/Persona_v2.java
@@ -0,0 +1,74 @@
+package Ejercicio_5;
+
+import java.time.LocalDate;
+
+/**
+ * Created by Stanislav Krastev
+ * TEMA11_Colecciones - Ejercicio_3
+ * Date: marzo / 2022
+ */
+public class Persona_v2 {
+    private String nombre, tlf, email;
+    private LocalDate fechaNacimiento;
+
+    public Persona_v2(String nombre, String tlf, String email, LocalDate fechaNacimiento) {
+        this.nombre = nombre;
+        this.tlf = tlf;
+        this.email = email;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    // GETTERS Y SETTERS
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTlf() {
+        return tlf;
+    }
+
+    public void setTlf(String tlf) {
+        this.tlf = tlf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Persona_v2 persona = (Persona_v2) o;
+
+        return nombre.equalsIgnoreCase(persona.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return nombre.toLowerCase().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Soy " + nombre + ", mi teléfono es " + tlf + " y mi email es " + email + ". Mi fecha de nacimiento es " + fechaNacimiento;
+    }
+}
