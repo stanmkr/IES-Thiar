55459505e5d450eaa73c54c203e12f9ee46ab21b
Stanislav Krastev <stani.mk22@gmail.com>
2022-10-11 13:11:13 +0200

    Añadiendo mejoras

--- 2nd Course/Projects/Acceso a Datos/TEMA 1 - OOP. Inheritance, Polymorphism, and Interfaces/Ejercicios Acepta ElReto/src/Ventas.java
+++ 2nd Course/Projects/Acceso a Datos/TEMA 1 - OOP. Inheritance, Polymorphism, and Interfaces/Ejercicios Acepta ElReto/src/Ventas.java
@@ -17,48 +17,55 @@ public class Ventas {
         Scanner scanner = new Scanner(System.in);
 
         ArrayList<String> diasDeLaSemana = new ArrayList<>(Arrays.asList("MARTES", "MIERCOLES", "JUEVES", "VIERNES", "SABADO", "DOMINMGO"));
-        double sumaTotal = 0, valor = 0, diaMenor, diaMayor;
+        double sumaTotal = 0, valor = 0, diaMenosVentas, diaMasVentas;
 
         while (valor != -1) {
             try {
                 int indiceMayor = 0, indiceMenor = 0;
+                System.out.print("Introduce una cantidad para el " + diasDeLaSemana.get(0)+": ");
                 valor = Double.parseDouble(scanner.nextLine());
-                diaMenor = valor;
-                diaMayor = valor;
+                diaMenosVentas = valor;
+                diaMasVentas = valor;
                 sumaTotal += valor;
                 boolean esMayor = false, esMenor = false;
 
                 for (int i = 1; i < diasDeLaSemana.size(); i++) {
+                    System.out.print("Introduce una cantidad para el " + diasDeLaSemana.get(i)+": ");
+
                     valor = Double.parseDouble(scanner.nextLine());
                     sumaTotal += valor;
-                    if (valor == diaMayor) {
+                    if (valor == diaMasVentas) {
                         esMayor = true;
-                    } else if (valor > diaMayor) {
-                        diaMayor = valor;
+                    } else if (valor == diaMenosVentas) {
+                        esMenor = true;
+                    } else if (valor > diaMasVentas) {
+                        diaMasVentas = valor;
                         indiceMayor = i;
                         esMayor = false;
-                    } else if (valor == diaMenor) {
-                        esMenor = true;
-                    } else if (valor < diaMenor) {
+
+                    } else if (valor < diaMenosVentas) {
+                        diaMenosVentas = valor;
                         indiceMenor = i;
                         esMenor = false;
                     }
                 }
-                if (esMenor || esMayor) {
+                if (esMenor || esMayor) { // si los dos son true hay empate
                     System.out.println("EMPATE");
                 } else {
                     boolean domingoMasventas = false;
-                    if (sumaTotal / 6 < valor) {
+                    double mediaSemanal = sumaTotal/6;
+                    if (mediaSemanal < valor) {
                         domingoMasventas = true;
                     }
                     if (domingoMasventas) {
-                        System.out.println(diasDeLaSemana.get(indiceMayor) + " " + diasDeLaSemana.get(indiceMenor) + " SI");
+                        System.out.printf("\nDia de más ventas: %s\nDía de menos ventas: %s\nSe han hecho más ventas el domingo que la media semanal (%.2f): SI\n",diasDeLaSemana.get(indiceMayor),diasDeLaSemana.get(indiceMenor),mediaSemanal);
                     } else {
-                        System.out.println(diasDeLaSemana.get(indiceMayor) + " " + diasDeLaSemana.get(indiceMenor) + " NO");
+                        System.out.printf("\nDia de más ventas: %s\nDía de menos ventas: %s\nSe han hecho más ventas el domingo que la media semanal (%.2f): NO\n",diasDeLaSemana.get(indiceMayor),diasDeLaSemana.get(indiceMenor),mediaSemanal);
                     }
                 }
             } catch (NumberFormatException e) {
-                System.out.println("Debes de introducir números.");
+                System.out.println("\nDebes de introducir números.\nEmpecemos de nuevo.\n");
+                sumaTotal = 0;
             }
         }
         scanner.close();
