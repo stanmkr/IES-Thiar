7cec1e1efbef12b91e5f0b609ea027808fc491db
Stanislav Krastev <stani.mk22@gmail.com>
2022-10-14 09:00:17 +0200

    Ejercicio de hilos con método de espera .join()

---
+++ 2nd Course/Projects/Programación de Servicios y Procesos/UNIDAD 1 - Programacion de Procesos/Tema2/src/pspT2/ejemplos/_1_lanzaHilos/LanzaHilos2.java
@@ -0,0 +1,65 @@
+package pspT2.ejemplos._1_lanzaHilos;
+
+import java.util.Random;
+
+class Hilo2 implements Runnable {
+
+    private final String nombre;
+
+    Hilo2(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public void run() {
+        int pausa = new Random().nextInt(500);
+        System.out.println("Hola, soy " + nombre + " con pausa de " + pausa);
+        try {
+            Thread.sleep(pausa);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.printf("Hilo %s terminado.\n", nombre);
+    }
+
+}
+
+public class LanzaHilos2 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        // ArrayList<Thread> arrayHilos = new ArrayList<>();
+        Thread[] arrayHilos = new Thread[10];
+        for (int i = 0; i < arrayHilos.length; i++) {
+            arrayHilos[i] = new Thread(new Hilo2("Hilo " + i));
+            arrayHilos[i].start();
+        }
+
+        for (Thread hilo : arrayHilos) {
+            try {
+                hilo.join();
+            } catch (InterruptedException e) {
+                System.out.println("Proceso interrumpido");
+            }
+        }
+        System.out.println("Proceso principal terminado");
+/*
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
+        try {
+            t1.join();
+            t2.join();
+            t3.join();
+            t4.join();
+        } catch (InterruptedException e) {
+            System.out.println("Proceso interrumpido");
+        }
+        System.out.println("Proceso principal terminado");*/
+    }
+
+}
