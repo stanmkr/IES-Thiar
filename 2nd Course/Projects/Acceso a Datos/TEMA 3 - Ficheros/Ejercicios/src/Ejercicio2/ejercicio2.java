5a31c491c17f6b5e6e7e23ccb87ee272231eed78
Stanislav Krastev <stani.mk22@gmail.com>
2022-11-22 11:38:16 +0100

    añadiendo el ejercicio2

---
+++ 2nd Course/Projects/Acceso a Datos/TEMA 3 - Ficheros/Ejercicios/src/Ejercicio2/ejercicio2.java
@@ -0,0 +1,15 @@
+package Ejercicio2;
+
+import java.io.File;
+
+/**
+ * AUTOR:Stanislav Krastev
+ * PROYECTO: Ejercicios
+ * FECHA: Noviembre / 2022
+ */
+public class ejercicio2 {
+    public static void main(String[] args) {
+        File f = new File("");
+        System.out.println(f.getAbsolutePath());
+    }
+}
