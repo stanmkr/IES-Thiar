426d823e33013d4ed6f4941e95b43fd94b36dfe4
Stanislav Krastev <stani.mk22@gmail.com>
2022-11-30 16:04:23 +0100

    El programa principal y retoques en la clase Cliente

--- 2nd Course/Projects/Acceso a Datos/TEMA 3 - Ficheros/Ejercicios4/src/Ejercicio1/Cliente.java
+++ 2nd Course/Projects/Acceso a Datos/TEMA 3 - Ficheros/Ejercicios4/src/Ejercicio1/Cliente.java
@@ -1,24 +1,36 @@
 package Ejercicio1;
 
+import java.io.Serializable;
+
 /**
  * AUTOR:Stanislav Krastev
  * PROYECTO: Ejercicios4
  * FECHA: Noviembre / 2022
  */
-public class Cliente {
+public class Cliente implements Serializable {
     private String nombre, direccion;
-    private int numCliente;
+    private   int numCliente = 1;
+    private static int identificador;
     private double saldo;
     private Cuenta estadoCuenta;
 
     //  CONSTRUCTOR
 
-    public Cliente(String nombre, String direccion, int numCliente, double saldo, Cuenta estadoCuenta) {
+    public Cliente(String nombre, String direccion, double saldo, Cuenta estadoCuenta) {
         this.nombre = nombre;
         this.direccion = direccion;
-        this.numCliente = numCliente;
+        numCliente = numCliente + identificador++;
         this.saldo = saldo;
         this.estadoCuenta = estadoCuenta;
 
+    }
+
+    public Cuenta getEstadoCuenta() {
+        return estadoCuenta;
+    }
+
+    @Override
+    public String toString() {
+        return "CLIENTE: " + nombre + " ID: " + numCliente + " DIRECCION: " + direccion + " SALDO: " + saldo + " ESTADO DE LA CUENTA BANCARIA: " + estadoCuenta;
     }
 }
---
+++ 2nd Course/Projects/Acceso a Datos/TEMA 3 - Ficheros/Ejercicios4/src/Ejercicio1/Programa.java
@@ -0,0 +1,43 @@
+package Ejercicio1;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * AUTOR:Stanislav Krastev
+ * PROYECTO: Ejercicios4
+ * FECHA: Noviembre / 2022
+ */
+public class Programa {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Cliente> listaClientes = new ArrayList<>();
+        listaClientes.add(new Cliente("Pepe Salazar", "Calle Sol", 500.5, Cuenta.AL_DIA));
+        listaClientes.add(new Cliente("Juan Perez", "Calle Luna", 100, Cuenta.ATRASADA));
+        listaClientes.add(new Cliente("Pepe Sanchez", "Calle a la calle", 10, Cuenta.DEUDOR));
+        listaClientes.add(new Cliente("Pablo Escobar", "Calle Marte", 222.75, Cuenta.AL_DIA));
+        listaClientes.add(new Cliente("Manolo el del bombo", "Calle Jupiter", 1230, Cuenta.DEUDOR));
+
+        System.out.println("Introduce el archivo que vas a crear: ");
+        //String nombreArchivo = scanner.nextLine();
+        String nombreArchivo = "src/Ejercicio1/CarpetaClientes/clientes deudores.txt";
+
+        for (Cliente cliente : listaClientes) {
+            if (cliente.getEstadoCuenta().equals(Cuenta.DEUDOR)){
+                try {
+                   FileOutputStream fileOutputStream = new FileOutputStream(nombreArchivo);
+                    ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+                    objectOutputStream.writeObject(cliente);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+        }
+    }
+}
