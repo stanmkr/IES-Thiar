c5633b1a2b13ae3ddf0d129345e7f57c2ccecc6a
Stanislav Krastev <stani.mk22@gmail.com>
2022-12-12 10:09:43 +0100

    Creando la clase que va a controlar el archivo

---
+++ 2nd Course/Projects/Acceso a Datos/TEMA 3 - Ficheros/PracticaObjetos/src/AgregarCliente/ControlArchivo.java
@@ -0,0 +1,24 @@
+package AgregarCliente;
+
+import java.util.ArrayList;
+
+/**
+ * AUTOR:Stanislav Krastev
+ * PROYECTO: PracticaObjetos
+ * FECHA: Diciembre / 2022
+ */
+public class ControlArchivo {
+
+    public static ArrayList<Cliente> listaClientes = new ArrayList<>();
+    //FacturaciónAddCliente f = new FacturaciónAddCliente();
+    public static void anyadir(String DNI, String nombreCliente, String direccionCliente, double saldo) {
+        if (saldo == 0){
+            ControlArchivo.listaClientes.add(new Cliente(DNI,nombreCliente,direccionCliente,saldo,"al dia"));
+        } else if (saldo < 0) {
+            ControlArchivo.listaClientes.add(new Cliente(DNI,nombreCliente,direccionCliente,saldo,"deudor"));
+        }else {
+            ControlArchivo.listaClientes.add(new Cliente(DNI,nombreCliente,direccionCliente,saldo,"positivo"));
+        }
+
+    }
+}
