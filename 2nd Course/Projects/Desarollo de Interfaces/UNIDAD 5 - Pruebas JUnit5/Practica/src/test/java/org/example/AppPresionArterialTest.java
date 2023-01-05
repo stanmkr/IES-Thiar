543cdc996d0a9bfa9513455ce035a7918fcfc316
Stanislav Krastev <stani.mk22@gmail.com>
2023-01-05 12:27:50 +0100

    Test hecho del método de medir la temperatura

--- 2nd Course/Projects/Desarollo de Interfaces/UNIDAD 5 - Pruebas JUnit5/Practica/src/test/java/org/example/AppPresionArterialTest.java
+++ 2nd Course/Projects/Desarollo de Interfaces/UNIDAD 5 - Pruebas JUnit5/Practica/src/test/java/org/example/AppPresionArterialTest.java
@@ -13,18 +13,33 @@ import org.junit.jupiter.params.provider.MethodSource;
 class AppPresionArterialTest {
     AppPresionArterial app = new AppPresionArterial();
 
+    /**
+     * Método para las pulsaciones en caso de que la casilla de Atleta SI esté seleccionada, del cual se van a extraer datos para hacer el test parametrizado de multiples valores
+     * @return Matriz de números, en la primera posicion devolverá el valor introducido junto a su valor esperado {introducido 39 .. esperado 3} y asi sucesivamente
+     */
     private static Integer[][] valoresPulsoAtletaTrue() {
         return new Integer[][]{
                 {39, 3}, {61, 4}, {50, 5}
         };
     }
 
+    /**
+     * Método para las pulsaciones en caso de que la casilla de Atleta NO esté seleccionada, del cual se van a extraer datos para hacer el test parametrizado de multiples valores
+     *
+     * @return Matriz de números, en la primera posicion devolverá el valor introducido junto a su valor esperado {introducido 58 .. esperado 0} y asi sucesivamente
+     */
     private static Integer[][] valoresPulsoAtletaFalse() {
         return new Integer[][]{
                 {59, 0}, {101, 1}, {70, 2}
         };
     }
 
+
+    /**
+     * El test del método de medir el pulso, en caso de que la casilla Atleta esté seleccionada
+     * @param entrada del método <valoresPulsoAtletaTrue> el primer valor de la matriz
+     * @param esperado del método <valoresPulsoAtletaTrue> el segundo valor de la matriz
+     */
     @ParameterizedTest
     @MethodSource("valoresPulsoAtletaTrue")
     void diagnosticoPulsoAtletaTrue(int entrada, int esperado) {
@@ -32,6 +47,11 @@ class AppPresionArterialTest {
 
     }
 
+    /**
+     * El test del método de medir el pulso, en caso de que la casilla Atleta NO esté seleccionada
+     * @param entrada del método <valoresPulsoAtletaTrue> el primer valor de la matriz
+     * @param esperado del método <valoresPulsoAtletaTrue> el segundo valor de la matriz
+     */
     @ParameterizedTest
     @MethodSource("valoresPulsoAtletaFalse")
     void diagnosticoPulsoAtletaFalse(int entrada, int esperado) {
@@ -39,8 +59,31 @@ class AppPresionArterialTest {
 
     }
 
-    @Test
-    void diagnosticoTemperatura() {
 
+
+    /**
+     * Método para la temperatura del cual se van a extraer datos para hacer el test parametrizado de multiples valores
+     * @return Matriz de números, en la primera posicion devolverá el valor introducido junto a su valor esperado {introducido 39 .. esperado 3} y asi sucesivamente
+     */
+    private static Double[][] valoresTemperatura() {
+        return new Double[][]{
+                {30.0, 0.0}, {36.0, 1.0}, {37.8, 2.0}, {39.0,3.0}
+        };
     }
+
+
+    /**
+     * l test del método de medir la temperatura
+     * @param entrada del método <valoresTemperatura> el primer valor de la matriz
+     * @param esperado del método <valoresTemperatura> el segundo valor de la matriz
+     */
+    @ParameterizedTest
+    @MethodSource("valoresTemperatura")
+    void diagnosticoTemperatura(double entrada, double esperado) {
+        Assertions.assertEquals(esperado,app.diagnosticoTemperatura(entrada));
+    }
+
+
+
+
 }
