8c17254d5c83e2697678d9eacea4801470f39739
Stanislav Krastev <stani.mk22@gmail.com>
2023-01-30 10:37:25 +0100

    App para practicar el selector de items

---
+++ 2nd Course/Projects/Desarollo de Interfaces/UNIDAD 7 - .NET - WPF/Practica 2 - App Selector/Practica 2 - App Selector/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Practica_2___App_Selector.dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practica_2___App_Selector
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Persona> listaPersonas = new List<Persona>();
+            listaPersonas.Add(new Persona("Pablo","Gonzalez"));
+            listaPersonas.Add(new Persona("Antonio", "Sanchez"));
+            foreach(Persona persona in listaPersonas)
+            {
+                ComboBoxItem comboBoxItem = new ComboBoxItem();
+                comboBoxItem.Content= persona;
+                comboBoxPersonas.Items.Add(comboBoxItem);
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem comboBoxItem = (ComboBoxItem)comboBoxPersonas.SelectedItem;
+            Persona persona = (Persona)comboBoxItem.Content;
+            labelNombre.Content = persona.Nombre;
+            labelApellidos.Content = persona.Apellidos;
+
+        }
+    }
+}
---
+++ 2nd Course/Projects/Desarollo de Interfaces/UNIDAD 7 - .NET - WPF/Practica 2 - App Selector/Practica 2 - App Selector/dto/Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_2___App_Selector.dto
+{
+    internal class Persona{
+        public String Nombre { get; set; }
+        public String Apellidos { get; set; }
+
+        public Persona(String nombre, String apellidos)
+        {
+            this.Nombre = nombre;
+            this.Apellidos = apellidos;
+
+        }
+        public override string ToString()
+        {
+            return Nombre + " " + Apellidos;
+        }
+    }
+
+}
